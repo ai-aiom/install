7aa6424e26cdea2a0d75a24c10f7a078f0bacde1
zhangli <zhangli11@asiainfo.com>
2015-10-12 15:34:53 +0800

    rpm包改为yum安装

--- gim-dependence/install.sh
+++ gim-dependence/install.sh
@@ -11,20 +11,8 @@ sed -n '1,/^exit 0$/!p' $0 >$install_dir/gim-dependence.tar.gz
 cd $install_dir
 tar -xzvf gim-dependence.tar.gz
 
-rpm -Uvh mysql-libs-5.1.73-5.el6_6.x86_64.rpm
-rpm -ivh apr-1.3.9-5.el6_2.x86_64.rpm
-rpm -ivh dejavu-lgc-sans-mono-fonts-2.30-2.el6.noarch.rpm
-rpm -ivh dejavu-sans-mono-fonts-2.30-2.el6.noarch.rpm
-rpm -ivh erlang-17.4-1.el6.x86_64.rpm
-rpm -ivh libconfuse-2.7-4.el6.x86_64.rpm
-rpm -ivh ganglia-3.7.1-2.el6.x86_64.rpm
-rpm -ivh rrdtool-1.3.8-7.el6.x86_64.rpm
-rpm -ivh ganglia-gmetad-3.7.1-2.el6.x86_64.rpm
-rpm -ivh mysql-5.1.73-5.el6_6.x86_64.rpm
-rpm -ivh perl-DBD-MySQL-4.013-3.el6.x86_64.rpm
-rpm -ivh mysql-server-5.1.73-5.el6_6.x86_64.rpm
-rpm -ivh rabbitmq-server-3.5.3-1.noarch.rpm
-rpm -ivh ganglia-gmond-3.7.1-2.el6.x86_64.rpm
+rm -rf /etc/yum.repos.d/*
+yum install -y *.rpm
 
 cp my.cnf /etc
 
